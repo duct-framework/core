6215682336c80e5d47986eea073b787db8f2f499
James Reeves <jreeves@weavejester.com>
2017-04-10 16:16:31 +0100

    Update project description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject duct/core "0.9.0-SNAPSHOT"
-  :description "Support library for the Duct template."
-  :url "https://github.com/weavejester/duct"
+  :description "The core library for the Duct framework"
+  :url "https://github.com/duct-framework/core"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
