c715bf7d23a713921e7977c93be72d2c3de3e30d
James Reeves <jreeves@weavejester.com>
2017-04-11 14:03:56 +0100

    Add Codox for API documentation

--- project.clj
+++ project.clj
@@ -5,4 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [integrant "0.3.3"]
-                 [meta-merge "1.0.0"]])
+                 [meta-merge "1.0.0"]]
+  :plugins [[lein-codox "0.10.3"]]
+  :codox {:output-path "codox"
+          :source-uri
+          "https://github.com/duct-framework/core/blob/{version}/{filepath}#L{line}"})
