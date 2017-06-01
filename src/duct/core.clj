9ee5a3af2b4d451715da1952f92721f5db2d5d46
James Reeves <jreeves@weavejester.com>
2017-06-01 15:27:30 +0100

    Add option to prep to limit namespace loads

--- src/duct/core.clj
+++ src/duct/core.clj
@@ -136,15 +136,23 @@
         (throw (missing-requirements-exception config modules applied)))
       config)))
 
+(defn- select-derived-keys [m ks]
+  (m/filter-keys #(some (partial isa? %) ks) m))
+
+(defn- load-namespaces [config keys]
+  (ig/load-namespaces (select-derived-keys config keys)))
+
 (defn prep
   "Prep a configuration, ready to be initiated. Key namespaces are loaded,
   resources included, and modules applied."
-  [config]
-  (-> config
-      (apply-includes (memoize read-config))
-      (doto ig/load-namespaces)
-      (apply-modules)
-      (doto ig/load-namespaces)))
+  ([config]
+   (prep config (keys config)))
+  ([config load-keys]
+   (-> config
+       (apply-includes (memoize read-config))
+       (doto (load-namespaces load-keys))
+       (apply-modules)
+       (doto (load-namespaces load-keys)))))
 
 (defn compile
   "Prep then initiate all keys that derive from `:duct/compiler`."
