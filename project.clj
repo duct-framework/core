f6f213d5dc3681f4afd402109de6e1b690d1cd6c
James Reeves <jreeves@weavejester.com>
2017-06-23 15:11:49 +0100

    Update Clojure version to 1.9.0-alpha17

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/duct-framework/core"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [integrant "0.4.0"]
                  [medley "1.0.0"]
                  [fipp "0.6.9"]]
