a697e8183450ca3d768c6a2b9ab1620630b6c44f
James Reeves <jreeves@weavejester.com>
2018-04-10 16:13:17 +0100

    Update Integrant to 0.7.0-alpha2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [integrant "0.7.0-alpha1"]
+                 [integrant "0.7.0-alpha2"]
                  [medley "1.0.0"]
                  [fipp "0.6.12"]]
   :plugins [[lein-codox "0.10.3"]]
